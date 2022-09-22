f56383170f23b2d25a82adfbcc3d0d276f00f2df
Fausto Obama <faustoobama@outlook.com>
2022-09-22 15:43:32 +0200

    Mejora de la piramide

--- index.php
+++ index.php
@@ -1,27 +1,3 @@
-<?php
-echo "EJERCICIOS <br><br>";
-
-function printPir(){
-    if($_GET['numero'] != 0 || $_GET['numero'] != ""){
-        $rows=$_GET['numero'];
-    }else{
-        $rows=0;
-        echo "<script>alert('Debes ingresar un valor valido')</script>";
-    }
-    for($row=1;$row<=$rows;$row++){
-        echo "<div class='maincont'>";
-        for($x=$row;$x<=$rows+1;$x++){
-            echo "<div class='void'></div>";
-        }
-        for($i=1;$i<=$row+($row-1);$i++){
-            echo "<div class='filled'></div>";
-        }
-        echo" </div>";
-    }
-}
-printPir();
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -31,34 +7,80 @@ printPir();
     <title>Piramide</title>
 
     <style>
-
+        *{
+            margin: 0;
+            padding: 0;
+        }
         .maincont{
-            --var-ancho: 50px;
+            --var-ancho: 25px;
             display: flex;
             width: auto;
         }
 
         .filled{
-            border: 1px solid black;
+            background: brown;
             width: var(--var-ancho);
             height: var(--var-ancho);
         }
         .void{
             width: var(--var-ancho);
             height: var(--var-ancho);
-            border: 1px solid transparent;
+        }
+        #elformulario{
+            width:500px;
+            min-height: 50px;
+            margin: 0 auto;
+            text-align: center;
+        }
+        form,input{
+            display: block;
+            margin: 9px auto;
+        }
+        input[value="enviar"]{
+            width: 80px;
+            height: 25px;
+            border-radius: 0px;
+        }
+        #piramide{
+            margin: 0 auto;
+            width: fit-content;
+            min-height: 200px;
         }
     </style>
-
 </head>
 
 <body>
-    <div>
+    <h2>Piramide</h2>
+    <div id="elformulario">
         <form action="./index.php" method="get">
-            <input type="text" name="numero" id="numero">
+            <input type="number" name="numero" id="numero">
             <input type="submit" value="enviar">
         </form>
     </div>
+    <?php
+
+function printPir(){
+    if($_GET['numero'] > 1){
+        $rows=$_GET['numero'];
+    }else{
+        $rows=0;
+        echo "<script>alert('Debes ingresar un valor valido, entre 2 y n')</script>";
+    }
     
+    echo "<div id='piramide'>";
+    for($row=1;$row<=$rows;$row++){
+        echo "<div class='maincont'>";
+        for($x=$row;$x<$rows;$x++){
+            echo "<div class='void'></div>";
+        }
+        for($i=1;$i<=$row+($row-1);$i++){
+            echo "<div class='filled'></div>";
+        }
+        echo" </div>";
+    }
+    echo "</div>";
+}
+printPir();
+?>
 </body>
 </html>
