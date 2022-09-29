2144d1438bf51b99172ad25748f35af58c16f267
Fausto Obama <Faustoobama@outlook.com>
2022-09-29 21:09:14 +0200

    Horario corregido y con colores

--- horario.php
+++ horario.php
@@ -8,25 +8,49 @@ $horario=[
     4 => ["Viernes", "DWES", "DWES", "DWES", "DESCANSO", "DWEC", "DWEC", "DWEC"]
 ];
 
+function printTd($e,$f){
+    switch($e){
+        case "DWEC":
+            echo "<td rowspan=".$f." class='lyellow'>".$e."</td>";
+        break;
+        case "DWES":
+            echo "<td rowspan=".$f." class='blue'>".$e."</td>";
+        break;
+        case "EIE":
+            echo "<td rowspan=".$f." class='yellow'>".$e."</td>";
+        break;
+        case "ITGS":
+            echo "<td rowspan=".$f." class='blue'>".$e."</td>";
+        break;
+        case "DAW":
+            echo "<td rowspan=".$f." class='purple'>".$e."</td>";
+        break;
+        case "DIW":
+            echo "<td rowspan=".$f." class='pink'>".$e."</td>";
+        break;
+        default:
+        echo "<td rowspan=".$f." class='rown'>".$e."</td>";
+
+    }
+}
+
 function printHorario(){
     global $horario;
     $longFilas=count($horario);
     $longCols=count($horario[0]);
-    $repet;
     $rowspan=1;
 
     for($cols=0; $cols < $longCols; $cols++){
         echo "<tr>";
         for($fila=0; $fila < $longFilas; $fila++){
 
-            if($cols < 4){
-                if($cols == 1 && $horario[$fila][$cols] == $horario[$fila][$cols+$rowspan]){
+                if(($cols == 5 || $cols == 1) && $horario[$fila][$cols] == $horario[$fila][$cols+$rowspan]){
                     if($horario[$fila][$cols] == $horario[$fila][$cols+$rowspan+1]){
                         $rowspan=3;
-                        echo "<td rowspan=".$rowspan.">".$horario[$fila][$cols]."</td>";
+                        printTd($horario[$fila][$cols],$rowspan);
                     }else {
                         $rowspan=2;
-                        echo "<td rowspan=".$rowspan.">".$horario[$fila][$cols]."</td>";
+                        printTd($horario[$fila][$cols],$rowspan);
                     }
                     $rowspan=1;
             
@@ -35,59 +59,18 @@ function printHorario(){
                     if($horario[$fila][$cols] != $horario[$fila][$cols-1]){
 
                         if($horario[$fila][$cols] == $horario[$fila][$cols+1]) {
-                            echo "<td rowspan='2'>".$horario[$fila][$cols]."</td>";
-                        }else{
-                            echo "<td rowspan='1'>".$horario[$fila][$cols]."</td>";
-                        }
-        
-                    }
-
-                }
-                
-            }else{
-                // if($cols == 5 && $horario[$fila][$cols] == $horario[$fila][$cols+$rowspan]){
-                //     $rowspan++;
-                //     if($horario[$fila][$cols] == $horario[$fila][$cols+$rowspan]){
-                //         $rowspan++;
-                //     }
-                // }else{
-                //     if($rowspan < $cols-4){
-    
-                //         if($cols != 4){
-                //             echo "<td rowspan=".$rowspan.">".$horario[$fila][$cols]."</td>";
-                //         }else{
-                //             if($fila == 0) echo "<td colspan='5'>".$horario[$fila][$cols]."</td>";
-                //         }
-    
-                //     }
-                // }
-
-                if($cols == 5 && $horario[$fila][$cols] == $horario[$fila][$cols+$rowspan]){
-                    if($horario[$fila][$cols] == $horario[$fila][$cols+$rowspan+1]){
-                        $rowspan=3;
-                        echo "<td rowspan=".$rowspan.">".$horario[$fila][$cols]."</td>";
-                    }else {
-                        $rowspan=2;
-                        echo "<td rowspan=".$rowspan.">".$horario[$fila][$cols]."</td>";
-                    }
-                    $rowspan=1;
-            
-                }else{
-
-                    if($horario[$fila][$cols] != $horario[$fila][$cols-1]){
-
-                        if($horario[$fila][$cols] == $horario[$fila][$cols+1]) {
-                            echo "<td rowspan='2'>".$horario[$fila][$cols]."</td>";
+                            $rowspan++;
+                            printTd($horario[$fila][$cols],$rowspan);
                         }else{
                             echo ($cols !=4 )
-                            ? "<td rowspan='1'>".$horario[$fila][$cols]."</td>"
-                            : (($fila == 0) ? "<td colspan='5'>".$horario[$fila][$cols]."</td>": "");
+                            ? printTd($horario[$fila][$cols],1)
+                            : (($fila == 0) ? "<td colspan='5' class='brown'>".$horario[$fila][$cols]."</td>": "");
                         }
+                        $rowspan=1;
         
                     }
 
                 }
-            }
 
         }
         echo "</tr>";
@@ -108,19 +91,29 @@ function printHorario(){
             text-align: center;
         }
         table{
+            box-shadow: 0 0 10px grey;
             border-collapse: collapse;
+            width: 1000px;
+            height: 340px;
         }
         td{
-            padding: 10px 30px;
-            border: 1px solid black;
+            padding: 10px 60px;
+            width: 100px;
+            height: 25px;
+            border: 3px solid white;
         }
+        .lyellow{background: #FFF79CFF;}
+        .yellow{background: #FFF370FF;}
+        .blue{background: #78C6FFFF;}
+        .pink{background: #FF8FE6FF;}
+        .purple{background: #BC87FFE0;}
+        .brown{background: #E6F7FFE0;}
+        .rown{background: #FFC6BDE0;}
     </style>
 </head>
 <body>
     <table>
-        <?php
-            printHorario();
-        ?>
+        <?php printHorario(); ?>
     </table>
 </body>
 </html>
