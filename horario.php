4ede9e32cc283e250e1dfce9ed9ae0f12c75de49
Fausto Obama <faustoobama@outlook.com>
2022-09-29 02:45:55 +0200

    Horario Finalizado

--- horario.php
+++ horario.php
@@ -1,25 +1,97 @@
 <?php
 
 $horario=[
-    0 => ["Lunes", "DWEC", "DWEC", "DEWC", "DESCANSO", "EIE", "EIE", "ITGS"],
+    0 => ["Lunes", "DWEC", "DWEC", "DWEC", "DESCANSO", "EIE", "EIE", "ITGS"],
     1 => ["Martes", "ITGS", "DAW", "DAW", "DESCANSO", "DIW", "DIW", "DIW"],
-    2 => ["Miercoles", "DAW", "DAW", "DAW", "DESCANSO", "DWES", "DWES", "DWES"],
+    2 => ["Miercoles", "DIW", "DIW", "DIW", "DESCANSO", "DWES", "DWES", "DWES"],
     3 => ["Jueves", "EIE", "DAW", "DAW", "DESCANSO", "DWES", "DWES", "DWES"],
     4 => ["Viernes", "DWES", "DWES", "DWES", "DESCANSO", "DWEC", "DWEC", "DWEC"]
 ];
 
-$laTabla="";
+function printHorario(){
+    global $horario;
+    $longFilas=count($horario);
+    $longCols=count($horario[0]);
+    $repet;
+    $rowspan=1;
 
-$longCols=count($horario);
-$longFilas=count($horario[0]);
+    for($cols=0; $cols < $longCols; $cols++){
+        echo "<tr>";
+        for($fila=0; $fila < $longFilas; $fila++){
 
-for($cols=0; $cols < $longFilas; $cols++){
-    for($fila=0; $fila < $longCols; $fila++){
-        if($cols != 4){
-            echo $horario[$fila][$cols]." <span>&nbsp;</span>";
+            if($cols < 4){
+                if($cols == 1 && $horario[$fila][$cols] == $horario[$fila][$cols+$rowspan]){
+                    if($horario[$fila][$cols] == $horario[$fila][$cols+$rowspan+1]){
+                        $rowspan=3;
+                        echo "<td rowspan=".$rowspan.">".$horario[$fila][$cols]."</td>";
+                    }else {
+                        $rowspan=2;
+                        echo "<td rowspan=".$rowspan.">".$horario[$fila][$cols]."</td>";
+                    }
+                    $rowspan=1;
+            
+                }else{
+
+                    if($horario[$fila][$cols] != $horario[$fila][$cols-1]){
+
+                        if($horario[$fila][$cols] == $horario[$fila][$cols+1]) {
+                            echo "<td rowspan='2'>".$horario[$fila][$cols]."</td>";
+                        }else{
+                            echo "<td rowspan='1'>".$horario[$fila][$cols]."</td>";
+                        }
+        
+                    }
+
+                }
+                
+            }else{
+                // if($cols == 5 && $horario[$fila][$cols] == $horario[$fila][$cols+$rowspan]){
+                //     $rowspan++;
+                //     if($horario[$fila][$cols] == $horario[$fila][$cols+$rowspan]){
+                //         $rowspan++;
+                //     }
+                // }else{
+                //     if($rowspan < $cols-4){
+    
+                //         if($cols != 4){
+                //             echo "<td rowspan=".$rowspan.">".$horario[$fila][$cols]."</td>";
+                //         }else{
+                //             if($fila == 0) echo "<td colspan='5'>".$horario[$fila][$cols]."</td>";
+                //         }
+    
+                //     }
+                // }
+
+                if($cols == 5 && $horario[$fila][$cols] == $horario[$fila][$cols+$rowspan]){
+                    if($horario[$fila][$cols] == $horario[$fila][$cols+$rowspan+1]){
+                        $rowspan=3;
+                        echo "<td rowspan=".$rowspan.">".$horario[$fila][$cols]."</td>";
+                    }else {
+                        $rowspan=2;
+                        echo "<td rowspan=".$rowspan.">".$horario[$fila][$cols]."</td>";
+                    }
+                    $rowspan=1;
+            
+                }else{
+
+                    if($horario[$fila][$cols] != $horario[$fila][$cols-1]){
+
+                        if($horario[$fila][$cols] == $horario[$fila][$cols+1]) {
+                            echo "<td rowspan='2'>".$horario[$fila][$cols]."</td>";
+                        }else{
+                            echo ($cols !=4 )
+                            ? "<td rowspan='1'>".$horario[$fila][$cols]."</td>"
+                            : (($fila == 0) ? "<td colspan='5'>".$horario[$fila][$cols]."</td>": "");
+                        }
+        
+                    }
+
+                }
+            }
+
         }
+        echo "</tr>";
     }
-    echo "<br>";
 }
 
 ?>
@@ -31,10 +103,24 @@ for($cols=0; $cols < $longFilas; $cols++){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Horario</title>
+    <style>
+        *{
+            text-align: center;
+        }
+        table{
+            border-collapse: collapse;
+        }
+        td{
+            padding: 10px 30px;
+            border: 1px solid black;
+        }
+    </style>
 </head>
 <body>
     <table>
-
+        <?php
+            printHorario();
+        ?>
     </table>
 </body>
 </html>
